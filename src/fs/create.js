c1d00a99cca501b8b186cfb0d58bcb8aa2bc3e46
Vladimir <noise123q@mail.ru>
2024-02-27 12:01:57 +0300

    feat: add create script

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, "/src/fs/files", "fresh.txt");
+
+  if (fs.existsSync(pathFile)) {
+    throw new Error("FS operation failed");
+  }
+  fs.appendFile(pathFile, "I am fresh and young", function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
 };
 
 await create();
