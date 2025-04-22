274ade3c2ae8b033d35e4b119fcc888e02f1a0f3
noisekov <noise123q@mail.ru>
2025-04-22 18:06:46 +0300

    feat: solve rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fsPromises from 'fs/promises'
+import path from 'path';
+
+const __dirname = path.resolve();
+const pathFile = (fileName) => path.join(__dirname, `/src/fs/files/${fileName}`);
+
+async function exists (path) {  
+  try {
+    await fsPromises.access(path)
+    return true
+  } catch {
+    return false
+  }
+}
+
 const rename = async () => {
-    // Write your code here 
+  const pathInitialFile = pathFile('wrongFilename.txt');
+  const pathRenameFile = pathFile('properFilename.md');
+
+  if (!await exists(pathInitialFile) || await exists(pathRenameFile)) {
+    throw new Error('FS operation failed')
+  }
+
+  fsPromises.rename(
+    pathInitialFile,
+    pathRenameFile
+  )
 };
 
 await rename();
