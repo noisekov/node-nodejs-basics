281353226d9e290735800f964d8d53f03f922c95
noisekov <noise123q@mail.ru>
2025-10-20 22:38:11 +0300

    refactor: change sync api to async

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,17 +1,25 @@
-import path from 'path';
-import { cpSync, existsSync, mkdirSync } from 'node:fs';
+import path from "path";
+import { cp, access } from "node:fs/promises";
 
 const copy = async () => {
   const __dirname = path.resolve();
-  const pathFileFrom = path.join(__dirname, "/src/fs", 'files');
-  const pathFileTo = path.join(__dirname, "/src/fs", 'files_copy');
+  const pathFileFrom = path.join(__dirname, "/src/fs", "files");
+  const pathFileTo = path.join(__dirname, "/src/fs", "files_copy");
 
-  if (existsSync(pathFileTo)) {
-    throw new Error('FS operation failed');
+  try {
+    await access(pathFileTo);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    const FILE_NOT_EXITS_ERR = "ENOENT";
+
+    if (err.code === FILE_NOT_EXITS_ERR) {
+      await cp(pathFileFrom, pathFileTo, {
+        recursive: true,
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
   }
-  
-  mkdirSync(pathFileTo);
-  cpSync(pathFileFrom, pathFileTo, { recursive: true });
 };
 
 await copy();
