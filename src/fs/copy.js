368998db14a13c862bfafd37c3a198eba2f14a3c
noisekov <noise123q@mail.ru>
2025-10-21 11:42:34 +0300

    refactor: chanege realsation copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -6,20 +6,22 @@ const copy = async () => {
   const pathFileFrom = path.join(__dirname, "/src/fs", "files");
   const pathFileTo = path.join(__dirname, "/src/fs", "files_copy");
 
-  try {
-    await access(pathFileTo);
+  if (!(await exists(pathFileTo))) {
+    await cp(pathFileFrom, pathFileTo, {
+      recursive: true,
+    });
+  } else {
     throw new Error("FS operation failed");
-  } catch (err) {
-    const FILE_NOT_EXITS_ERR = "ENOENT";
-
-    if (err.code === FILE_NOT_EXITS_ERR) {
-      await cp(pathFileFrom, pathFileTo, {
-        recursive: true,
-      });
-    } else {
-      throw new Error("FS operation failed");
-    }
   }
 };
+
+async function exists(path) {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 await copy();
