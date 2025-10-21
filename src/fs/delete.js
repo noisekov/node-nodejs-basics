fcf00d472373fdc68ae08c35a2daae4bb7e99777
noisekov <noise123q@mail.ru>
2025-10-21 15:21:57 +0300

    feat: add delete implementation

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import { access, unlink } from "fs/promises";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, "/src/fs/files", "fileToRemove.txt");
+
+  if (await exists(pathFile)) {
+    await unlink(pathFile);
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
+
+async function exists(path) {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
