d79dbb6d4a6ef51ddfa96c7973e97ee8580b5536
noisekov <noise123q@mail.ru>
2025-10-23 22:43:03 +0300

    refactor: read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,11 +1,11 @@
-import fs from "node:fs";
+import { createReadStream } from "node:fs";
 import path from "path";
 import { stdin, stdout } from "node:process";
 
 const read = async () => {
   const __dirname = path.resolve();
   const pathFile = path.join(__dirname, "/src/streams/files", "fileToRead.txt");
-  const readStream = fs.createReadStream(pathFile);
+  const readStream = createReadStream(pathFile);
 
   readStream.pipe(process.stdout);
 };
