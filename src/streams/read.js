27bcc95aa50277edce041aca46feed35983fce01
noisekov <noise123q@mail.ru>
2025-10-23 22:42:07 +0300

    feat: solve read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from "node:fs";
+import path from "path";
+import { stdin, stdout } from "node:process";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, "/src/streams/files", "fileToRead.txt");
+  const readStream = fs.createReadStream(pathFile);
+
+  readStream.pipe(process.stdout);
 };
 
 await read();
