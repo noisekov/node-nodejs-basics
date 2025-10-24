a145ddd6d22bdf743af88217c629d71dac520454
noisekov <noise123q@mail.ru>
2025-10-24 18:58:13 +0300

    feat: add write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from "node:fs/promises";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+  process.stdin.setEncoding("utf8");
+  const __dirname = path.resolve();
+  const pathFile = path.join(
+    __dirname,
+    "/src/streams/files",
+    "fileToWrite.txt"
+  );
+
+  process.stdin.on("readable", async () => {
+    let chunk;
+
+    while ((chunk = process.stdin.read()) !== null) {
+      await fs.writeFile(pathFile, chunk, { flag: "a+" });
+    }
+  });
 };
 
 await write();
