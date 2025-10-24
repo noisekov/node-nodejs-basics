5a680ef083261dc168f02794b1eb266f15357f83
noisekov <noise123q@mail.ru>
2025-10-24 23:22:49 +0300

    feat: solve decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = path.resolve();
+  const pathFileFrom = path.join(__dirname, "/src/zip/files", "archive.gz");
+  const pathFileTo = path.join(
+    __dirname,
+    "/src/zip/files",
+    "fileToCompress.txt"
+  );
+
+  const readStream = fs.createReadStream(pathFileFrom);
+  const writeStream = fs.createWriteStream(pathFileTo);
+  const gunzipStream = zlib.createGunzip();
+  readStream.pipe(gunzipStream).pipe(writeStream);
 };
 
 await decompress();
