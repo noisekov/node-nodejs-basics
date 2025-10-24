4fc6024e9c31b07fb5703f96694b192c6fe802dc
noisekov <noise123q@mail.ru>
2025-10-24 21:32:11 +0300

    feat: solve transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseText = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedText = chunk.toString().split("").reverse().join("") + "\n";
+      callback(null, reversedText);
+    },
+  });
+
+  process.stdin.pipe(reverseText).pipe(process.stdout);
 };
 
 await transform();
