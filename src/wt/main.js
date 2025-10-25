c809da90541b553ff84e8c42cc42ce767d583461
noisekov <noise123q@mail.ru>
2025-10-25 11:55:52 +0300

    feat: solver worker threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from "worker_threads";
+import path from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const CPU_CORES = 6;
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, "/src/wt", "worker.js");
+  const resultsArr = [];
+  let startFibonacciNumber = 10;
+
+  for (let core = 1; core <= CPU_CORES; core++) {
+    const workerCPU = new Worker(pathFile, {
+      workerData: startFibonacciNumber,
+    });
+
+    resultsArr.push(
+      new Promise((resolve) => {
+        workerCPU.on("message", async (message) => {
+          resolve({ status: "resolved", data: message });
+        });
+
+        workerCPU.on("error", async (message) => {
+          resolve({ status: "error", data: null });
+        });
+      })
+    );
+
+    startFibonacciNumber++;
+  }
+
+  const result = await Promise.all(resultsArr);
+  console.log(result);
+
+  return result;
 };
 
 await performCalculations();
